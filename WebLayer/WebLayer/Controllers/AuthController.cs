a1c4c9e59d3b8868ac5eb8c1d74b71916a5ca4a8
Kristofferkirk <kristoffer_kg@hotmail.se>
2016-12-25 18:00:47 +0100

    Fixed validation for AuthController reguarding password

--- WebLayer/WebLayer/Controllers/AuthController.cs
+++ WebLayer/WebLayer/Controllers/AuthController.cs
@@ -31,36 +31,41 @@ namespace WebLayer.Controllers
                 var emailCheck = db.Users.FirstOrDefault(u => u.Email == model.Email);
                 var getPassword = db.Users.Where(u => u.Email == model.Email).Select(u => u.Password);
                 var materializePassword = getPassword.ToList();
-                var password = materializePassword[0];
-                var decryptedPassword = CustomDecrypt.Decrypt(password);
-
-                if (model.Email != null && model.Password == decryptedPassword)
+                if (!materializePassword.Equals(null))
                 {
-                    var getName = db.Users.Where(u => u.Email == model.Email).Select(u => u.Name);
-                    var materializeName = getName.ToList();
-                    var name = materializeName[0];
+                    var password = materializePassword[0];
+                    var decryptedPassword = CustomDecrypt.Decrypt(password);
+                    if (model.Email != null && model.Password == decryptedPassword)
+                    {
+                        var getName = db.Users.Where(u => u.Email == model.Email).Select(u => u.Name);
+                        var materializeName = getName.ToList();
+                        var name = materializeName[0];
 
-                    var getCity = db.Users.Where(u => u.Email == model.Email).Select(u => u.City);
-                    var materializeCity = getCity.ToList();
-                    var city = materializeCity[0];
+                        var getCity = db.Users.Where(u => u.Email == model.Email).Select(u => u.City);
+                        var materializeCity = getCity.ToList();
+                        var city = materializeCity[0];
 
-                    var getEmail = db.Users.Where(u => u.Email == model.Email).Select(u => u.Email);
-                    var materializeEmail = getEmail.ToList();
-                    var email = materializeEmail[0];
+                        var getEmail = db.Users.Where(u => u.Email == model.Email).Select(u => u.Email);
+                        var materializeEmail = getEmail.ToList();
+                        var email = materializeEmail[0];
 
-                    var identity = new ClaimsIdentity(new[] {
+                        var identity = new ClaimsIdentity(new[] {
                     new Claim(ClaimTypes.Name, name),
                     new Claim(ClaimTypes.Email, email),
                     new Claim(ClaimTypes.Country, city)
-                },
-                        "ApplicationCookie");
-                    var ctx = Request.GetOwinContext();
-                    var authManager = ctx.Authentication;
+                    },
+                            "ApplicationCookie");
+                        var ctx = Request.GetOwinContext();
+                        var authManager = ctx.Authentication;
 
-                    authManager.SignIn(identity);
+                        authManager.SignIn(identity);
 
-                    return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "Home");
+                    }
                 }
+                
+
+               
             }
             ModelState.AddModelError("", "Invalid email or password");
             return View(model); //Should always be  declared on the end of an action method. Laddar viewen Login med modelen som parameter
