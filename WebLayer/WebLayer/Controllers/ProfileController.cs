94e9cb4d8f53ad500862e790d08f50876aab4aac
Kristofferkirk <kristoffer_kg@hotmail.se>
2017-01-04 20:17:11 +0100

    Added profile upload and display picture

--- WebLayer/WebLayer/Controllers/ProfileController.cs
+++ WebLayer/WebLayer/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,5 +14,26 @@ namespace WebLayer.Controllers
         {
             return View();
         }
+       [HttpPost]  
+public ActionResult Index(HttpPostedFileBase file)  
+{  
+    if (file != null && file.ContentLength > 0)  
+        try 
+        {  
+            string path = Path.Combine(Server.MapPath("~/images/profile"),  
+                                      "profile.jpg");  
+            file.SaveAs(path);  
+            ViewBag.Message = "File uploaded successfully";  
+        }  
+        catch (Exception ex)  
+        {  
+            ViewBag.Message = "ERROR:" + ex.Message.ToString();  
+        }  
+    else 
+    {  
+        ViewBag.Message = "You have not specified a file.";  
+    }  
+    return View();  
+}
     }
 }
