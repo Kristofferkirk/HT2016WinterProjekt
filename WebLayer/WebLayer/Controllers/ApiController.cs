37b825e26418a2201a248e1d5a9ca00bf028d0cf
LundkvistViktor <lundkvist.viktor@gmail.com>
2017-01-06 16:05:58 +0100

    api base

---
+++ WebLayer/WebLayer/Controllers/ApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebLayer.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getProfile(int Queryid)
+        {
+            Boolean succeded = true;
+            if (succeded)
+            {
+                return Json(new
+                {
+                    Succeded = false, //sätt värden från profile modellen
+                    Id = Queryid,
+                    Name = "@nameval",
+                    Age = "@ageval",
+                    Etc = "etc etc"
+
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    succeded = false
+                });
+
+            }
+        }
+    }
+}
